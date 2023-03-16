e2bb4f12a0c335392a84bf98b7de0c6c254688ee
github <filipe.boeck@hotmail.com>
2023-03-16 19:15:40 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by wolf on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
